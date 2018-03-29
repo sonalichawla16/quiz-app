92a283860bafe0d1db059a4a448f9d808b4d424c
sonalichawla16 <36824225+sonalichawla16@users.noreply.github.com>
2018-03-30 02:10:34 +0530

    Quiz controller crud operations (#95)

--- quiz-app-api/app/controllers/api/QuizController.js
+++ quiz-app-api/app/controllers/api/QuizController.js
@@ -10,25 +10,40 @@ class QuizController extends ResourceController {
     // }        
 }
 var qc = new QuizController(Quiz);
-//qc.list();
-// quiz = {
-//     create: (req, res) => {
-//         qc.create({ "quizName": "New Quiz", "year": "2018" }).then((result) => {
-//             res.send(result);
-//         });
-//     },
-//     list: (req, res) => {
-//         qc.index().then((result) => {
-//             res.send(result);
-//         });
-//     },
-//     show: (req, res) =>{
-//         qc.show(req.params._id).then((result)=>{
-//             res.send(result);
-//         });        
-//     }
+qc.list();
+ quiz = {
+     create: (req, res) => {
+     	var quizObj = {
+	quizName: req.body.quizName,
+	year: req.body.year
+}
+        qc.create(quizObj).then((result) => {
+             res.send(result);
+         });
+     },
+     list: (req, res) => {
+         qc.index().then((result) => {
+             res.send(result.sort());
+         });
+     },
+     show: (req, res) =>{
+         qc.show(req.params._id).then((result)=>{
+             res.send(result);
+         });        
+     },
+     update: (req, res) => {
+   
+        qc.update(req).then((result) => {
+            res.send(result);
+        });
+    },
+    delete: (req, res) =>{
+        qc.delete(req.params._id).then((result)=>{
+            res.send(result);
+        });        	
+    }
 
 
-// }
+ }
 
 module.exports = qc
