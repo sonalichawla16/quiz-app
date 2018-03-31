55b3d7a42ad4e0754c1970365f19ee30b3e209b3
nehachauhan14 <34521600+nehachauhan14@users.noreply.github.com>
2018-04-01 00:00:55 +0530

    Added Category Add pop up (#92)
    * Veiw for Add Category added , LEFT : Need to integrate API
    
    * Veiw for Add Category added - Resolved Conflict

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpModule } from '@angular/http';
 
 // services
 import {HttpWrapperService} from './services/http-wrapper.service';
+import {AuthService} from './services/authservice.service';
 
 // Components
 import {AppComponent} from './app.component';
@@ -17,22 +18,19 @@ import {NavBarComponent} from './nav-bar/nav-bar.component';
 import {ManageQuizComponent} from './manage-quiz/manage-quiz.component';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {SampleComponent} from './sample/sample.component';
-
 import { AddQuestionsComponent } from './add-questions/add-questions.component';
-
 import { CategorySelectComponent } from './category-select/category-select.component';
-
-import {AuthService} from './services/authservice.service';
 import { TeamListComponent } from './team-list/team-list.component';
+import { PostComponent } from './post/post.component';
+import {QuizSelectComponent} from './quiz-select/quiz-select.component';
+import { AddNewCategoryDialogComponent } from './add-new-category-dialog/add-new-category-dialog.component';
 import { AddNewTeamDialogComponent, DIALOG_DATA } from './add-new-team-dialog/add-new-team-dialog.component';
 
 // Material Imports
 import {MatSelectModule, MatFormField, MatButtonModule, MatCheckboxModule, MatRadioModule,
   MatCardModule, MatExpansionModule, MatIconModule, MatFormFieldModule,
-  MatInputModule, MatDatepickerModule, MatNativeDateModule, MatChipsModule ,MatDialogModule} from '@angular/material';
+  MatInputModule, MatDatepickerModule, MatNativeDateModule, MatChipsModule , MatDialogModule} from '@angular/material';
 
-import { PostComponent } from './post/post.component';
-import {QuizSelectComponent} from './quiz-select/quiz-select.component';
 
 @NgModule({
   declarations: [
@@ -41,16 +39,18 @@ import {QuizSelectComponent} from './quiz-select/quiz-select.component';
     RegisterComponent,
     ManageQuizComponent,
     QuizSelectComponent,
+    QuizComponent,
     NotFoundComponent,
     SampleComponent,
     AddQuestionsComponent,
+
     QuizComponent,
     LoginComponent,
     CategorySelectComponent,
     PostComponent,
     TeamListComponent,
     AddNewTeamDialogComponent,
-    PostComponent
+    AddNewCategoryDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -79,28 +79,23 @@ import {QuizSelectComponent} from './quiz-select/quiz-select.component';
       {
         path: '',
         component: RegisterComponent
-      },
-    {
+      }, {
       path: 'managequiz',
       component : ManageQuizComponent
-    },
-    {
+    }, {
       path: 'home' ,
       component: RegisterComponent
-    } ,
-    {
+    }, {
       path: 'sample' ,
       component: SampleComponent
-    } ,
-    {
+    }, {
       path: 'addques' ,
       component: AddQuestionsComponent
     } ,
     {
       path: 'quiz-select',
       component: QuizSelectComponent
-    },
-    {
+    }, {
       path: 'login' ,
       component: LoginComponent
     }, {
@@ -109,24 +104,20 @@ import {QuizSelectComponent} from './quiz-select/quiz-select.component';
     }, {
       path: 'team',
       component: TeamListComponent
-    },
-    {
+    }, {
       path: 'post',
       component: PostComponent
-    },
-      {
+    }, {
         path: '**',
         component: NotFoundComponent
-    }
-    ]),
-    HttpModule
+    }])
   ],
-  providers: [
+providers: [
     HttpWrapperService,
     AuthService
   ],
-  entryComponents: [AddNewTeamDialogComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ AddNewTeamDialogComponent, AddNewCategoryDialogComponent ],
 })
 
 export class AppModule {
--- front-end/src/app/services/authservice.service.ts
+++ front-end/src/app/services/authservice.service.ts
@@ -42,7 +42,6 @@ export class AuthService {
 
     isLoggedIn() {
         return tokenNotExpired();
-
     }
 
     registration(userData) {
