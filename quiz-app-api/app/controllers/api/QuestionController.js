21c56d027ea7a285b3bea71928159c3ad24d673e
Aakash Verma <35327196+aakashvermaee@users.noreply.github.com>
2018-04-04 00:32:53 +0530

    Update QuestionController.js

--- quiz-app-api/app/controllers/api/QuestionController.js
+++ quiz-app-api/app/controllers/api/QuestionController.js
@@ -140,42 +140,37 @@ let question = {
 
   // Pagination  ex: ?page=2&limit=10&perPage=10
   "questionsPagination": (req, res, next) => {
-    var obj = {
+    var queryString = {
       "page": (req.query.page || 1),
-      "limit": req.query.limit,
-      "perPage": req.query.perPage
+      "limit": (req.query.limit || 10),
+      "perPage": (req.query.perPage || 10)
     };
 
-    doPagination(obj);
+    doPagination(queryString);
 
-    function doPagination(o) {
+    function doPagination(param) {
       Question
         .find({})
-        .skip((o.perPage * o.page) - o.limit)
-        .limit(o.limit)
-        .exec((err, questions) => {
+        .skip((param.perPage * param.page) - param.limit)
+        .limit(param.limit)
+        .exec(function(err, questions) {
           Question
             .count()
-            .exec((err, count) => {
+            .exec(function(err, count) {
               if (err) {
-                res
-                  .status(404)
-                  .json({ "status": "BAD REQUEST" });
+                return next(err);
               }
               res
                 .status(200)
                 .json({
                   "status": "SUCCESS",
-                  "page": o.page,
-                  "perPage": o.perPage,
-                  "limit": o.limit,
-                  "questions": (function () {
-                    let x = ((o.perPage * o.page) - o.limit);
-                    return this.listAllQuestions.slice(x, (o.limit + x));
-                  })()
-                })
-            })
-        })
+                  "page": param.page,
+                  "perPage": param.perPage,
+                  "limit": param.limit,
+                  "questions": questions
+                });
+            });
+        });
     };
   }
 };
