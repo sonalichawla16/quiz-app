4764d30712a692795bac13c0ca8f390c19a65cbb
nehachauhan14 <34521600+nehachauhan14@users.noreply.github.com>
2018-04-03 15:16:35 +0530

    Integrated Add Category API  (#97)
    * Veiw for Add Category added , LEFT : Need to integrate API
    
    * Veiw for Add Category added - Resolved Conflict
    
    * Completed API integration for Adding Category
    
    * Changed Http to httpWrapperService

--- front-end/src/app/models/appSettings.ts
+++ front-end/src/app/models/appSettings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-    base : 'http://vishalranjan.in:1830/';
+    base = 'http://vishalranjan.in:1830/';
     adminUrl =  'http://vishalranjan.in:1830/quizapp/api/category';
 }
